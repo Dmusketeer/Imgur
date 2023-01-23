7814327a6d5777c0c86af81f2b42a401de877f2f
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-23 18:13:15 +0530

    imgur clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import Navbar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarImgur from './components/navbar';
 import './App.css';
-import SignUp from './components/pages/SignUp';
-import Login from './components/pages/Login';
-import Home from './components/pages/Home';
-import New from './components/pages/New';
 
 function App() {
   return (
-    <>
-      <Router className="main">
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/sign-up' component={SignUp} />
-          <Route path='/newpost' component={New} />
-        </Switch>
-      </Router>
-    </>
+    <div className='App'>
+    <NavbarImgur/>
+    </div>
   );
 }
 
 export default App;
-
